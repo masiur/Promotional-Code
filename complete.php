4d909cece6ed878fc9f9fb1acd9801331b6b7925
Masiur Rahman Siddiki <masiur.sust@csesociety.org>
2015-03-28 22:45:34 +0600

    Email box & Verification added

--- complete.php
+++ complete.php
@@ -1,20 +1,38 @@
-<!DOCTYPE html>
+<!DOCTYPE html> <.>
 <html>
 <head>
 	<title>Complete Your Registration</title>
 </head>
 <body>
 	<?php
-	$ukeyErr = $ukey = "";
+
+	$ukeyErr = $ukey = $email = $emailErr = "";
+
 	if ($_SERVER["REQUEST_METHOD"] == "POST") {
+		if (empty($_POST['email'])) {
+			$emailErr = "Email is required";
+		} else {
+			$email = test_input($_POST['email']);
+			if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+				$emailErr= "Please, Provide a Valid Email Address";
+			}
+		}
+
 		if (empty($_POST["ukey"])) {
 			$ukeyErr = "Key is reuired";
 		} else {
 			$ukey = test_input($_POST["ukey"]);
 		}
 	}
+	function test_input($data) {
+		$data = trim($data);
+		return $data;
+	}
+
 	?>
 	<form method="post" action="<?php ($_SERVER["PHP_SELF"]);?>">
+		<label for="email">Give Your Email Address once you Provided</label>
+		<input type="email" name="email" id="email" required>
 		<label for="ukey" >Provide Your Unique Key & Complete Registation Process</label><br><br>
 
 		<input type="text" id="ukey" name="ukey" required>
