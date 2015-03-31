e8c71c6bacad054526df80a46294d07872b92ab0
Masiur Rahman Siddiki <masiur.sust@csesociety.org>
2015-04-01 01:27:00 +0600

    php code moved

--- complete.php
+++ complete.php
@@ -3,57 +3,15 @@
 	<title>Complete Your Registration</title>
 </head>
 <body>
-	<?php
-
-	$ukeyErr = $ukey = $email = $emailErr = "";
-
-	if ($_SERVER["REQUEST_METHOD"] == "POST") {
-		if (empty($_POST['email'])) {
-			$emailErr = "Email is required";
-		} else {
-			$email = test_input($_POST['email']);
-			if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-				$emailErr= "Please, Provide a Valid Email Address";
-			}
-		}
-
-		if (empty($_POST["ukey"])) {
-			$ukeyErr = "Key is reuired";
-		} else {
-			$ukey = test_input($_POST["ukey"]);
-		}
-	}
-	function test_input($data) {
-		$data = trim($data);
-		return $data;
-	}
-
-	?>
-	<form method="post" action="<?php ($_SERVER["PHP_SELF"]);?>">
-		<label for="email">Give Your Email Address once you Provided</label>
-		<input type="email" name="email" id="email" required>
+	<form method="post" action="action.php">
+		<label for="email">Give Your Email Address once you Provided</label><br><br>
+		<input type="email" name="email" id="email" required><br><br>
 		<label for="ukey" >Provide Your Unique Key & Complete Registation Process</label><br><br>
 
 		<input type="text" id="ukey" name="ukey" required>
-		<span class="error">* <?php echo $ukeyErr;?></span>
-		<br>
+		<span class="error"></span>
+		<br><br>
 		<input type="submit" name="submit" value="Complete Registration">
 	</form>
-	<?php 
-		$conn = mysqli_connect("localhost","root","","payment_system");
-		if ($conn->connect_error) {
-			die("connection failed: " . $conn->connect_error);
-		}
-
-		$sql = "UPDATE str_key
-				SET is_used = '1'
-				WHERE key_uniq = '$ukey'
-				AND is_used ='0'";
-		
-		} else {
-			echo "Error: " . $sql. "<br>". $conn->error;
-		}
-		$conn->close();
-	?>
 </body>
 </html>
