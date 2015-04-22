15b7351259e1cb18fc18a5fc6e31923398b5017c
Masiur Rahman Siddiki <masiur.sust@csesociety.org>
2015-04-23 01:05:58 +0600

    added

--- string_generator.php
+++ string_generator.php
@@ -32,10 +32,8 @@ try {
 	VALUES ('$val','0')";
 	// use exec because no results are returned
 	$conn->exec($sql);
-	echo "New record created successfully";
-	
+	echo "New record created successfully.";
 	}
-
 	
 catch(PDOException $e) {
 	echo $sql . "<br>" . $e->getMessage();
