c7da070a6f6685284cb103cf987c6bbfcdd55852
Masiur Rahman Siddiki <masiur.sust@csesociety.org>
2015-04-24 11:00:58 +0600

    uncommited

--- string_generator.php
+++ string_generator.php
@@ -32,7 +32,7 @@ try {
 	VALUES ('$val','0')";
 	// use exec because no results are returned
 	$conn->exec($sql);
-	echo "New record created successfully.";
+	echo "New record created successfully";
 	}
 	
 catch(PDOException $e) {
